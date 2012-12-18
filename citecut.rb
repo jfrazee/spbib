f172b76767fc27a0472179c878abf62965f700f9
Joey Frazee <joseph.frazee@gmail.com>
2012-12-17 19:04:54 -0600

    Simplified citecut.rb and fixed errors in spbib.bib

--- citecut.rb
+++ citecut.rb
@@ -1,27 +1,20 @@
-require 'set'
 require 'bibtex'
 
 # Usage: ruby citecut.rb <latex> <bib>
 
-# Get the citekeys used in the LaTeX doc
-citekeys = Set.new
-open(ARGV[0]).readlines.each do |line|
-  line.scan(/\\\w*cite\w*{([\w\d,:-]+)}/) do |c|
-    citekeys.merge c[0].split(',')
-  end
-end
+# Get the cite keys from the LaTeX document
+citekeys = open(ARGV[0]).read.scan(/\\\w*cite\w*{([^},]+,?)+}/).flatten.map(&:strip).uniq.sort
 
 # Create a new .bib file w/ only the used entries
 bib = BibTeX.open(ARGV[1])
-citekeys.sort.each do |c|
-  if bib[c].nil?     
-    bib.each do |entry|
-      if entry.key.downcase == c.downcase
-        entry.key = c 
-        warn "updating citekey: #{c}"
-        break 
-      end
+citekeys.each do |citekey|
+  bib.each do |entry|
+    if entry.key.downcase == citekey.downcase
+      entry.key = citekey
+      warn "updating citekey: #{citekey}"
+      break
     end
-  end
-  puts "#{bib[c]}\n"
+  end if bib[citekey].nil?
+
+  puts "#{bib[citekey]}\n"
 end
